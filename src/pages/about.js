84f692e6887591323aecf829469bdfba70f74ca4
Filippo 20u <filippo.20u@gmail.com>
2022-01-19 11:22:15 -0800

    page 2

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,9 @@
 // Step 1: Import React
 import * as React from 'react'
+// import { Link } from 'gatsby'
+import Layout from '../components/layout'
+
+
 
 // Step 2: Define your component
 const AboutPage = () => {
@@ -7,7 +11,13 @@ const AboutPage = () => {
     <main>
       <title>About Me</title>
       <h1>About Me</h1>
+      {/* <Link to="/">Back to Home</Link> */}
+
+      <Layout pageTitle="About Me">
       <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    </Layout>
+
+     
     </main>
   )
 }
